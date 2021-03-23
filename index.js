9aa0e24a1bf5ae4a47e05944f50eee949e5f50e2
piemekanika@hotmail.com <piemekanika@hotmail.com>
2021-03-23 18:48:57 +0700

    added a check if the value is actual number

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const isNumber = require('is-number');
+
 /**
  * Checks if the given value equals eleven.
  *
+ * @param {number} value to check.
  * @returns {boolean} is given value equals 11.
  */
 function isEleven(value) {
-	return 11 === value;
+	if (!isNumber(value)) {
+		return false;
+	}
+
+	return 11 === Number(value);
 };
 
 module.exports.isEleven = isEleven;
-
