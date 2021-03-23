4dac8e29f9541277c39e64a3ce45dc6083dd42ef
piemekanika <piemekanika@hotmail.com>
2021-03-23 13:40:10 +0700

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# What is this?
+
+Library that helps you to check if value equals 11.
+
+# Installation
+
+Hope you how to install packages.
+
+# Usage
+
+@todo :)
+
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Checks if the given value equals eleven.
+ *
+ * @returns {boolean} is given value equeals 11.
+ */
+function isEleven(value) {
+	return 11 === value;
+};
+
+module.exports.isEleven = isEleven;
+
