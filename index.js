f97658d0af656c84a89eea424c56838993cc7f1e
piemekanika@hotmail.com <piemekanika@hotmail.com>
2021-03-23 22:50:25 +0700

    refactor

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const isNumber = require('is-number');
 
+const ELEVEN = 11;
+
 /**
- * Checks if the given value equals eleven.
+ * Checks if the given value equals eleven. Works with both
+ * numbers and strings.
  *
- * @param {number} value to check.
+ * It uses `is-number` library to prevent situations
+ * like `Number([11]) === 11`.
+ *
+ * @example
+ * isEleven(0b1011); // true
+ * isEleven(' 11'); // true
+ * isEleven('11'); // true
+ * isEleven(11); // true
+ *
+ * @example
+ * isEleven(12); // false
+ * isEleven('12'); // false
+ * isEleven([11]); // false
+ *
+ * @param {number} value - value to check.
  * @returns {boolean} is given value equals 11.
  */
 function isEleven(value) {
@@ -11,7 +28,7 @@ function isEleven(value) {
 		return false;
 	}
 
-	return 11 === Number(value);
+	return ELEVEN === Number(value);
 };
 
 module.exports.isEleven = isEleven;
